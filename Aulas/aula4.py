fdb1d832426ea48d9cfd9bddc671254c805af4fe
IvanAires23 <ivanaires32@gmail.com>
2025-04-21 20:47:18 -0300

    feat: aula 3

---
+++ Aulas/aula4.py
@@ -0,0 +1,29 @@
+print("Ola")
+print('Ola')
+
+w = "Curso de Python3"
+
+print(w)
+"""Comentario"""
+
+a = """olá,
+este é o curso de python!"""
+
+print(a)
+
+# Metodos de strings
+
+b = " Ola Mundo "
+
+print(b.strip())
+
+c = "OLA MUNDO"
+print(c.lower())
+
+d = "ola mundo"
+print(d.upper())
+
+e = "Ola"
+f = "Mundo"
+g = e + " " + f
+print(g)
