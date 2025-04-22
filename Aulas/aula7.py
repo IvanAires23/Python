294b6089a1c481fdd2bf3388f346952c3b948c92
IvanAires23 <ivanaires32@gmail.com>
2025-04-22 12:00:00 -0300

    feat: aula7

---
+++ Aulas/aula7.py
@@ -0,0 +1,27 @@
+"""Escopo de variaveis"""
+
+"""
+Variaveis globais
+
+Variaveis locais
+"""
+
+x = 5
+print(x)
+
+def funcao():
+    x = 3
+    print(x)
+
+funcao()
+
+y = "Gabriel"
+z = 1.74
+t = "000.000.000-00"
+l = 32
+
+
+nome = "Gabriel"
+altura = 1.74
+cpf = "000.000.000-00"
+idade = 32
